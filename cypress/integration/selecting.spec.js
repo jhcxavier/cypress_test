b29f778bcafb8c72ccbd9353776fffa18ccd3977
João Henrique Xavier <jhcxavier@gmail.com>
2020-01-21 15:25:58 -0500

    add testing using special data attributes 'data-cy'

--- cypress/integration/selecting.spec.js
+++ cypress/integration/selecting.spec.js
@@ -4,30 +4,35 @@ describe("Text box with mas characters", ()=>{
     it("Display what I want ", ()=>{
         cy.visit('http://localhost:3000/example-3');
 
-        cy.get('span')
-            .eq(1)
+        // The recommended way of of selecting elements in Cypress is by using special data 
+        // attributes on the elements we want to test. which is the data-cy attribute and you can 
+        //assign any unique value to it
+        
+
+        cy.get('[data-cy="last-name-chars-left-count"]')
+            
             .invoke('text')
             .should('equal', '15');
 
-        cy.get('input').eq(1).type('hello');
+        cy.get('[data-cy="input-last-name"]').type('hello');
 
-        cy.get('span')
-            .eq(1)
+        cy.get('[data-cy="last-name-chars-left-count"]')
+          
             .invoke('text')
             .should('equal', '10');
 
-        cy.get('input').eq(1).type(' my friend');
+        cy.get('[data-cy="input-last-name"]').type(' my friend');
 
-        cy.get('span')
-            .eq(1)  
+        cy.get('[data-cy="last-name-chars-left-count"]')
+            
             .invoke('text')
             .should('equal', '0');
     });
 
     it('Prevents the user from typing more characters when max is exceeded', ()=>{
-        cy.visit('http://localhost:3000/example-2');
-        cy.get('input').type('jsjsjsjsjsjsjsjsjsjsjasasasas')
-        cy.get('input')
+        cy.visit('http://localhost:3000/example-3');
+        cy.get('[data-cy="input-last-name"]').type('jsjsjsjsjsjsjsjsjsjsjasasasas')
+        cy.get('[data-cy="input-last-name"]')
             .should('have.attr', 'value', 'jsjsjsjsjsjsjsj')
     })
 })
